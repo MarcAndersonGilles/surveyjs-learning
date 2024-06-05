021a7ca4af1684618c2e36df8cc2f22dbf60db3d
marc <hornil6748@gmail.com>
2024-06-05 15:45:03 -0400

    add survey make route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import SurveyjsRenderJson from "@/components/survey-js/SurveyjsRenderJson"
-import SurveyJsWrapper from "@/components/survey-js/SurveyjsWrapper"
 
 function page() {
     return (
         <div className="h-full">
-            {/* <SurveyJsWrapper /> */}
             <SurveyjsRenderJson />
         </div>
     )
---
+++ app/survey/page.tsx
@@ -0,0 +1,12 @@
+
+import SurveyJsWrapper from "@/components/survey-js/SurveyjsWrapper"
+
+function page() {
+    return (
+        <div className="h-full">
+            <SurveyJsWrapper />
+        </div>
+    )
+}
+
+export default page
